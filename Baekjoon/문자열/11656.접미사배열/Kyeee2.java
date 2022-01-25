ac145c6b8c1b8b0ad8798ce0c3c64f18b546aaa9
kyeee2 <youngeun0208@naver.com>
2022-01-25 23:21:59 +0900

    [백준] 11656 접미사배열

---
+++ Baekjoon/문자열/11656.접미사배열/Kyeee2.java
@@ -0,0 +1,39 @@
+package algorithm_me;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Kyeee2 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		String [] words = new String[str.length()];
+		
+		// 접미사 배열로 만들기
+		for(int i = 0; i < str.length(); i++) {
+			words[i] = str.substring(i);
+		}
+		
+		// 사전순으로 버블정렬
+		for(int i = 0; i < words.length - 1; i++) {
+			for(int j = 0; j < words.length - 1; j++) {
+				if(words[j].compareTo(words[j + 1]) > 0) {
+					String temp = words[j];
+					words[j] = words[j + 1];
+					words[j + 1] = temp;
+				}
+			}
+		}
+		
+		for(String word: words) {
+			System.out.println(word);
+		}
+	}
+
+}
