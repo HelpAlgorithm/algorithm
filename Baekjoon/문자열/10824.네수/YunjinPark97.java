012b41b28bca1fec748e7924581a1a0bf8576b4f
박윤진 <pyjpyj97@naver.com>
2022-01-27 07:41:00 +0900

    [백준] 10824 네수

--- Baekjoon/문자열/10824.네수/YunjinPark97.java
+++ Baekjoon/문자열/10824.네수/YunjinPark97.java
@@ -1,33 +1,18 @@
 package stringManipulation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-public class Baekjoon10820_문자열분석 {
+public class Baekjoon10824_네수 {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String st;
-		while ((st = br.readLine()) != null){
-			int[] answer = new int[4];
-			for (int i = 0; i < st.length(); i++) {
-				char letter = st.charAt(i);
-				if (letter >= 97 && letter < 123) {
-					answer[0] += 1;
-				}else if (letter >= 65 && letter < 91) {
-					answer[1] += 1;
-				}else if (letter >= 48 && letter < 58) {
-					answer[2] += 1;
-				}else {
-					answer[3] += 1;
-				}
-			}
-			for (int ans: answer) {
-				System.out.printf("%d ", ans);
-			}
-			System.out.println();
-		}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		String c = sc.next();
+		String d = sc.next();
+		String abSum = a+b;
+		String cdSum = c+d;
+		System.out.println(Long.parseLong(abSum) + Long.parseLong(cdSum));
 	}
 
 }
