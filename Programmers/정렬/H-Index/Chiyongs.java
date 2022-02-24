ba11fb9575bbcc3176c38fa6ee80735a6c677b12
chiyongs <tlscldyd@gmail.com>
2022-02-24 21:12:43 +0900

    [프로그래머스] 정렬 H-Index

--- Programmers/정렬/H-Index/Chiyongs.java
+++ Programmers/정렬/H-Index/Chiyongs.java
@@ -1,18 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        
-        for(int i=0, size=commands.length;i<size;i++) {
-            int start = commands[i][0]-1;
-            int end = commands[i][1];
-            int k = commands[i][2]-1;
-            
-            int[] temp = Arrays.copyOfRange(array, start, end);
-            Arrays.sort(temp);
-            answer[i] = temp[k];            
+    public int solution(int[] citations) {
+        int answer = 0, max = 0;        
+        Arrays.sort(citations);        
+
+        int h = 0;
+        int size = citations.length;
+        while(true) {
+            int cnt = 0;
+            for(int i=0;i<size;i++) {
+                if(citations[i] >= h) {
+                    cnt++; // h번 이상 인용된 논문 갯수
+                }
+            }
+            if(cnt < h) break; // h번 이상 인용된 논문 갯수가 h보다 작으면 조건 불충족
+            // h번 이상 인용된 논문이 h편 이상이어야 함!
+
+            if(cnt >= h && size-cnt <= h) {
+            	// h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용인 경우
+                if(max < h) {
+                    max = h; // max값 갱신
+                }
+            }
+            h++;
         }
+        answer= max;
         return answer;
     }
 }
