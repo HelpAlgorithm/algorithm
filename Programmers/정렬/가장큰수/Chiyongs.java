218d8493bbd0d267e56ed1a850980dfc1c3b7c4e
chiyongs <tlscldyd@gmail.com>
2022-02-25 15:16:21 +0900

    [프로그래머스] 정렬 가장 큰 수

--- Programmers/정렬/가장큰수/Chiyongs.java
+++ Programmers/정렬/가장큰수/Chiyongs.java
@@ -1,18 +1,46 @@
 import java.util.*;
 
-class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        
-        for(int i=0, size=commands.length;i<size;i++) {
-            int start = commands[i][0]-1;
-            int end = commands[i][1];
-            int k = commands[i][2]-1;
-            
-            int[] temp = Arrays.copyOfRange(array, start, end);
-            Arrays.sort(temp);
-            answer[i] = temp[k];            
+class Solution { 
+
+    static class MyNumber implements Comparable<MyNumber>{
+        String value;
+        int org;
+
+        public MyNumber(String v,int org) {
+            this.value = v;
+            this.org = org;
         }
+
+        // 비교하는 로직을 생각해내는 게 제일 힘들었다.
+        @Override
+        public int compareTo(MyNumber o) {
+            return (o.value + this.value).compareTo(this.value + o.value);
+        }
+    }
+
+    public String solution(int[] numbers) {
+        String answer = "";
+
+        int maxLen = 0;
+
+        MyNumber[] temp = new MyNumber[numbers.length];
+
+        for(int i=0,size=numbers.length;i<size;i++) {
+            String str = String.valueOf(numbers[i]);            
+            temp[i] = new MyNumber(str,numbers[i]);          
+        }
+
+        Arrays.sort(temp);
+
+        for(int i=0,size=numbers.length;i<size;i++) {            
+            answer = answer + String.valueOf(temp[i].org);
+        }
+        
+        // 맨 앞이 '0'인 경우는 '0'만 계속 들어온 경우이다.
+        if(answer.charAt(0) == '0') {
+            return "0";
+        }
+
         return answer;
     }
 }
